5a63fd8d12263b8b083c6ede9055de01bc5bfeba
fernanaldana07-dotcom <fenanaldana07@gmail.com>
2026-09-25 09:50:02 -0600

    Merge: resuelto conflicto entre hotfix/fix-error-500 y feature/status-endpoint

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    nombre = "Estudiante"  # Corrección para evitar el Error 500
+    nombre = "Estudiante"
     return f"<h1>Bienvenido al portal universitario, {nombre}!</h1>"
 
 @app.route('/api/status')
